57d3d3316c12a5c1c3d5f33f6fcd1df286c81aaa
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-16 21:03:24 +0100

    add template validation and React setup in project generation command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Command } from 'commander';
 import * as path from 'path';
 import * as fs from 'fs';
+import { execSync  } from "child_process"
+import { Setup_React_app } from './templates/react.template.js';
 
 const program = new Command();
 
@@ -11,15 +13,53 @@ program
 program.command('generate <template> <projectName>')
   .description('Generate a new project based on a template')
   .action((template, projectName) => {
-    const templateDir = path.join(__dirname, 'templates', template);
-    const projectDir = path.join(process.cwd(), projectName);
+    const allowedTemplates = [
+      {
+        key:"Front-end",
+         flags:["React","Vue", "Angular"]
+      }, 
+      {
+        key:"Back-end",
+        flags:["Nest","Express"]
+      }, 
+      {
+        key:"Full-Stack",
+        flags:["Next","Nuxt"]
+      }, 
+      {
+        key:"Lib",
+        flags:["Simple"]
+      },
+      {
+        key:"Static",
+        flags:["Gatsby", "Hugo","Astro"]
+      }
+      ];
 
-    if (!fs.existsSync(templateDir)) {
-      console.error(`Template "${template}" does not exist.`);
-      process.exit(1);
+
+    //const templateDir = path.join(__dirname, 'templates', template);
+    // time to make a switch cases on each templates:
+    const arrTemp = template.split(":");
+    const appType = arrTemp[0];
+    const flag = arrTemp[1];
+    const isThere:boolean = allowedTemplates.filter(temp => temp.key === appType).length !==0;
+    if(!isThere){
+      console.error("Template not found ");
+      return ;
     }
 
-    fs.mkdirSync(projectDir, { recursive: true });
+    console.log(arrTemp);
+    const projectDir:string = path.join(process.cwd(), projectName);
+    console.log("Creating New Folder : ",  projectDir );
+    
+    // add steps : 
+    if(appType === "Front-end" && flag==="React"){
+      const isReactSetedUp:boolean = Setup_React_app(projectName);
+      if(!isReactSetedUp) return;
+    }
+
+    // fs.mkdirSync(projectDir, { recursive: true });
+    console.log(projectDir, "Cretated successfully :) ");
 
     function copyFiles(srcDir: string, destDir: string) {
       const entries = fs.readdirSync(srcDir, { withFileTypes: true });
@@ -37,7 +77,7 @@ program.command('generate <template> <projectName>')
       }
     }
 
-    copyFiles(templateDir, projectDir);
+    //copyFiles(templateDir, projectDir);
     console.log(`Generated project "${projectName}" based on template "${template}".`);
   });
 
