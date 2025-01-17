79d97d6c2465f97083770b20af32ddbe5103269a
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-17 18:32:12 +0100

    feat: add project name validation for Vue application setup

--- src/templates/vue.template.ts
+++ src/templates/vue.template.ts
@@ -1,9 +1,15 @@
 import { execSync } from "child_process";
+import { isValidProjectName } from "../utils/facades/validatePath.facade.js";
 
 
 export const Setup_Vue_app  = (projectName:string):boolean =>{
     try {
         console.log("Creating new Vue typescript application using vite integration : ");
+        // add validation of project name / folder :
+        if (!isValidProjectName(projectName)) {
+            return false;
+        }
+        
         execSync(`bun create vite ${projectName} --template vue-ts`, { stdio: 'inherit'});
         console.log('Installing dependencies...'); 
         execSync(`cd ${projectName} && bun install`, { stdio: 'inherit'});
