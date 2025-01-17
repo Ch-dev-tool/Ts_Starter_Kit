9249f0a196274d5c4987a64a4cc6f33374be947f
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-17 18:27:39 +0100

    feat: add Angular application setup function using Angular CLI

---
+++ src/templates/angular.template.ts
@@ -0,0 +1,30 @@
+import { execSync } from "child_process";
+import { mkdirSync } from "fs";
+import path from "path";
+import { isValidProjectName } from "../utils/facades/validatePath.facade.js";
+
+
+export const setupAngularApp = (projectName: string): boolean => {
+    try {
+        console.log("Creating new Angular TypeScript application using Angular CLI integration:");
+        console.log(projectName);
+
+        if (!isValidProjectName(projectName)) {
+            return false;
+        }
+
+        const projectPath = path.resolve(projectName);
+        const projectNameOnly = path.basename(projectPath);
+
+        mkdirSync(projectPath, { recursive: true });
+        execSync(`bunx @angular/cli new ${projectNameOnly} --defaults --skip-install --style=css --routing=true --skip-tests`,
+            { stdio: 'inherit', cwd: path.dirname(projectPath) });
+        console.log('Installing dependencies...');
+        execSync(`bun install`, { stdio: 'inherit', cwd: projectName });
+
+        return true;
+    } catch (error) {
+        console.error("Error setting up an Angular template project using Angular CLI provider:", error);
+        return false;
+    }
+};
