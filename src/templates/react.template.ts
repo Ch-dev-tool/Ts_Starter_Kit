aa8237aa4ab47a98e73a9b91a46a0be23e2d0892
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-17 18:31:59 +0100

    feat: add project name validation for React application setup

--- src/templates/react.template.ts
+++ src/templates/react.template.ts
@@ -1,10 +1,18 @@
 import { execSync } from "child_process";
+import { isValidProjectName } from "../utils/facades/validatePath.facade.js";
 
 
 export const Setup_React_app = (projectName:string):boolean=>{
     try {
         // execute vite script to setup a new react ts project : 
         console.log("Creating new React typescript application using vite integration : ");
+
+        // add validation of project name :
+        if (!isValidProjectName(projectName)) {
+            return false;
+        }
+        
+
         execSync(`bun create vite ${projectName} --template react-ts`, { stdio: 'inherit'});
         console.log('Installing dependencies...'); 
         execSync(`cd ${projectName} && bun install`, { stdio: 'inherit'});
