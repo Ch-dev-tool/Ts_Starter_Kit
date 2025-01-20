1424a843a6b51329a7452150fb55e62bfe7eed43
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-20 21:41:02 +0100

    feat: add API setup function for Nest.js backend

---
+++ src/templates/back.template.ts
@@ -0,0 +1,18 @@
+import { Setup_Nest_App } from "./back/nest.template.js";
+
+
+
+
+export const Setup_Api = (flag:string,projectName:string)=>{
+    // sitch over the flag :
+    switch( flag ){
+        case "Nest":
+            // call nest js setup command : 
+        const isNestSetup:boolean = Setup_Nest_App(projectName);
+        if(!isNestSetup){
+            console.error("Failed to setup a Nes backend :( ");
+            return;
+        }
+        break;
+    }
+}
