9f20e9428232a6f40661517761447110aba9a1ea
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-21 17:14:01 +0100

    feat: enhance Nest application setup by validating project name and creating directory

--- src/templates/back/nest.template.ts
+++ src/templates/back/nest.template.ts
@@ -1,4 +1,7 @@
 import { execSync } from "child_process";
+import { isValidProjectName } from "../../utils/facades/validatePath.facade.js";
+import path from "path";
+import { mkdirSync } from "fs";
 
 
 
@@ -6,12 +9,30 @@ import { execSync } from "child_process";
 export const Setup_Nest_App = (projectName:string):boolean =>{
     try{
         console.log("Creating new Nest application using nest/Cli integration : ");
+        // check the project name :
+        console.log(projectName);
+        const appName = path.basename(projectName);
+        const directory:string = path.dirname(projectName);
+        // console.table(
+        //     {
+        //         appName,
+        //         directory
+        //     }
+        // );
+        if(!isValidProjectName(projectName)){
+            return false;
+        }
+        // create the directory if is not exist :
+        mkdirSync(directory, { recursive: true });
+        // move to the directory and create the project :
+        process.chdir(directory);
         // insallt nest cli 
         execSync(`bun add -g @nestjs/cli`);
-        execSync(`nest new ${projectName}`, {stdio:"inherit"});
+        execSync(`nest new ${appName} --package-manager bun`, {stdio:"inherit"});
         return true;
     }catch( error ){
         console.log("error setting up a nest template project using nest cli integration ");
+        console.log(error);
         return false;
     }
 };
