78bec88f785f8e4c5749c90a23dc34f56da490c7
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-21 14:10:24 +0100

    feat: integrate Nest CLI for automated application setup

--- src/templates/back/nest.template.ts
+++ src/templates/back/nest.template.ts
@@ -1,3 +1,4 @@
+import { execSync } from "child_process";
 
 
 
@@ -5,9 +6,12 @@
 export const Setup_Nest_App = (projectName:string):boolean =>{
     try{
         console.log("Creating new Nest application using nest/Cli integration : ");
+        // insallt nest cli 
+        execSync(`bun add -g @nestjs/cli`);
+        execSync(`nest new ${projectName}`, {stdio:"inherit"});
+        return true;
     }catch( error ){
         console.log("error setting up a nest template project using nest cli integration ");
         return false;
     }
-    return true;
-}
+};
