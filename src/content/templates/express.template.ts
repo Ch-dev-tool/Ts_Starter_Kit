b88b27a6e967e357075c59b5c7722391d242b28c
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-22 22:07:44 +0100

    feat: update Express template to export app and port, and create index file template

--- src/content/templates/express.template.ts
+++ src/content/templates/express.template.ts
@@ -1,19 +1,19 @@
 export const Express_Template = `
+    
         import express, { Request, Response } from 'express';
+        // import routes form ./routes
+        // import controllers from ./controllers
+        // import middlewares from ./middlewares
+        // import database config from ./config/database
+        // import models from ./config/models
         
         const app = express();
         const port = process.env.PORT || 3000;
 
         app.use(express.json());
 
-        app.get('/', (req: Request, res: Response) => {
-            res.send('Hello, world!');
-        });
-
-        app.listen(port, () => {
-            console.log(\`Server is running on port \${port}\`);
-        });
-        `;
+        export { app,port };
+`;
 
 export const Express_Env_Template = `
         # Environment
@@ -33,3 +33,12 @@ export const Express_Env_Template = `
         JWT_SECRET=your_jwt_secret
         JWT_EXPIRATION=3600
         `;
+
+
+
+export const indexFile_Template = `
+        import { app, port } from "./src/express.app";
+        app.listen(port, () => {
+            console.log(\`Server is running on port \${port}\`);
+        });
+`;
