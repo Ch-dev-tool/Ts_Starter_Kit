ed36be41dabe0e3fb2773295c612eb749d87787a
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-22 21:05:34 +0100

    feat: enhance Copy_TsConfig function to read template from file and ensure target directory creation

--- src/content/tsConfig.copy.ts
+++ src/content/tsConfig.copy.ts
@@ -1,34 +1,27 @@
 // a function take a folder -> create a tsconfig file and past the content there :
-import fs from "fs";
+import fs, { mkdirSync } from "fs";
 import { CreateFileFactory } from "../utils/factory/file.factory.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
+export const Copy_TsConfig = (folder: string): boolean => {
+    // Ensure the folder exists
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    console.log("folder", __dirname);
+    const tsConfigPath = path.join(__dirname, "templates", "tsconfig.template.json");
+    console.log("tsConfigPath", tsConfigPath);
+    const targetDirectory = path.resolve(folder);
+    mkdirSync(targetDirectory, { recursive: true });
 
-
-export const Copy_TsConfig = (folder:string):boolean =>{
-
-    const tsConfigContent = `{
-    "compilerOptions": {
-        "target": "ES6",                      // Specify ECMAScript target version
-        "module": "commonjs",                 // Specify module code generation
-        "strict": true,                       // Enable all strict type-checking options
-        "esModuleInterop": true,              // Enable compatibility with CommonJS modules
-        "skipLibCheck": true,                 // Skip type checking of declaration files
-        "outDir": "./dist",                   // Redirect output structure to the directory
-        "rootDir": "./src",                   // Specify the root directory of input files
-        "resolveJsonModule": true,            // Include modules imported with .json extension
-        "noImplicitAny": true,                // Raise error on expressions and declarations with an implied "any" type
-        "sourceMap": true                     // Generates corresponding .map file for debugging
-    },
-    "include": ["src/**/*"],                // Include files in the project
-    "exclude": ["node_modules", "dist"]     // Exclude files from the project
-    }`
-    // create the file :
-    try{
-        CreateFileFactory(folder,"tsconfig.json",tsConfigContent);
-        return true;
-    }catch( error ){
-        console.log("error creating tsconfig file : ", error);
-        return false;
+    const tsConfigContent = fs.readFileSync(tsConfigPath, "utf-8");
+    console.table(tsConfigContent);
+    // // // Create the file
+     try {
+         CreateFileFactory(folder, "tsconfig.json", tsConfigContent);
+         return true;
+     } catch (error) {
+         console.log("Error creating tsconfig file:", error);
+         return false;
     }
-}
-
+};
