55ae88e2caa110f30d0c183a9f304e1b3464a824
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-22 22:07:20 +0100

    feat: enhance Express app setup to include index.ts and package.json generation

--- src/templates/back/express.template.ts
+++ src/templates/back/express.template.ts
@@ -5,7 +5,7 @@ import { execSync } from "child_process";
 import { Copy_TsConfig } from "../../content/tsConfig.copy.js";
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
-import { Express_Env_Template, Express_Template } from "../../content/templates/express.template.js";
+import { Express_Env_Template, Express_Template, indexFile_Template } from "../../content/templates/express.template.js";
 
 export const Setup_Express_App = (projectName: string): boolean => {
     try {
@@ -13,7 +13,7 @@ export const Setup_Express_App = (projectName: string): boolean => {
 
         const appName = path.basename(projectName);
         const directory: string = path.dirname(projectName);
-        // validate project name :
+        // validate project name : 
         if (!isValidProjectName(projectName)) return false;
         // create project folder if there is no folder with the same name:
         mkdirSync(directory, { recursive: true });
@@ -24,48 +24,52 @@ export const Setup_Express_App = (projectName: string): boolean => {
         process.chdir(appName);
         // setup bun express application  :
         execSync(`bun init -y`);
-        // install dependencies :
+        // install dependencies : 
         execSync("bun add express@next");
         execSync("bun add -d @types/express @types/node");
         // time to overwrite the tsconfig.json file :
         const rootProjectFolder = path.resolve(
             path.dirname(fileURLToPath(import.meta.url)),
             "../../../");
-        const tsConfigPath = path.join(rootProjectFolder,
-             "src", 
-             "content", 
-             "templates", 
-             "tsconfig.template.json"
-        );
-
+        const tsConfigPath = path.join(rootProjectFolder, "src", "content", "templates", "tsconfig.template.json");
+        const pakcagePath = path.join(rootProjectFolder, "src", "content", "templates", "package.template.json");
         const tsConfigTemplate = readFileSync(
             tsConfigPath,
             "utf-8"
         );
+        const packageTemplate = readFileSync(
+            pakcagePath,
+            "utf-8"
+        );
+
+
 
         writeFileSync("tsconfig.json", tsConfigTemplate);
-
+        writeFileSync("index.ts", indexFile_Template)
+        writeFileSync("package.json", packageTemplate);
+        // re run bun install to install the new dependencies :
+        execSync("bun install");
         writeFileSync(".env", Express_Env_Template);
         writeFileSync(".env.dev", Express_Env_Template);
-        // time to setup project folder :
+        // time to setup project folder : 
         // create src folder :
-        const srcPath = path.join(projectName, "src");
-        mkdirSync(srcPath, { recursive: true });
-
+        mkdirSync("src", { recursive: true });
+        process.chdir("src");
         // create src/index.ts file :
-        const indexPath = path.join(srcPath, "index.ts");
-        writeFileSync(indexPath, Express_Template);
-
+        writeFileSync("express.app.ts", Express_Template);
         // create additional directories :
-        const directories:string[] = [
+        const directories = [
             "routes", 
             "controllers", 
             "services", 
             "middlewares", 
             "config/database", 
-            "config/models"
+            "config/models", 
+            "shared/types", 
+            "shared/enums",
+            "shared/dto"
         ];
-        directories.forEach(dir => mkdirSync(path.join(srcPath, dir), { recursive: true }));
+        directories.forEach(dir => mkdirSync(dir, { recursive: true }));
         console.log("Express application setup complete.");
         return true;
     } catch (error) {
