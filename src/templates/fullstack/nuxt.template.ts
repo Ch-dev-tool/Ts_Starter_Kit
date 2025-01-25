2cc02c33177e4e1095c46a2cbc72fd25d91d384d
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-25 19:08:33 +0100

    feat: enhance Nuxt.js setup template with improved CLI command and environment variable configuration

--- src/templates/fullstack/nuxt.template.ts
+++ src/templates/fullstack/nuxt.template.ts
@@ -8,9 +8,11 @@ export const Setup_Nuxt_app = (projectName: string): boolean => {
         console.log("Creating new Nuxt.js application using nuxt cli integration : ");
         // add validation of project name / folder :
         if(!isValidProjectName(projectName)) return false;
-        const command = `bunx nuxi init ${projectName} --pm bun --no-git --ts --eslint --tailwind --src-dir --app --turbopack --import-alias "@/components/*" --no-participate`;
-        execSync(command, { stdio: 'inherit'});
-        return true;
+        // set the bun env variables :
+        execSync('bun add -g nuxi@3.20.0 && set NUXT_PACKAGE_MANAGER=bun', { stdio: 'inherit'});
+        const command = `bunx nuxi init ${projectName} --ts --eslint --tailwind --app --turbopack --import-alias "@/components/*" --no-participate  --no-telemetry --no-analytics  --package-manager bun --yes`;
+        execSync(`set NUXT_PACKAGE_MANAGER=bun && ${command}`, { stdio: 'inherit'});
+        return true;    
     } catch (error) {
         console.log("error setting up a nuxt template project using nuxt cli integration :( ");
         return false;
