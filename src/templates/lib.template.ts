5015c53bb6b82ad3c62b45dbb871d6badb3c9cf1
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-25 19:33:09 +0100

    feat: add Setup_Lib_App function for library application setup template

---
+++ src/templates/lib.template.ts
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+export const Setup_Lib_App = (flag: string, projectName: string) => {
+    switch( flag){
+        case "Simple":
+            // call simple lib setup command : 
+            console.log("Simple lib template not found");
+            break;
+    }
+};
