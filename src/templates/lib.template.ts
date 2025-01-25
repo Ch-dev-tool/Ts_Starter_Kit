7594800a0770b099d7b33430bdf924fca83b1619
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-01-25 20:35:12 +0100

    feat: integrate Setup_Simple_Lib_App for improved simple library project setup

--- src/templates/lib.template.ts
+++ src/templates/lib.template.ts
@@ -1,3 +1,4 @@
+import { Setup_Simple_Lib_App } from "./lib/simpleLib.template.js";
 
 
 
@@ -8,7 +9,11 @@ export const Setup_Lib_App = (flag: string, projectName: string) => {
     switch( flag){
         case "Simple":
             // call simple lib setup command : 
-            console.log("Simple lib template not found");
+            const isSimpleSetup:boolean = Setup_Simple_Lib_App(projectName);
+            if(!isSimpleSetup){
+                console.error("Failed to setup a Simple lib project :( ");
+                return;
+            };
             break;
     }
 };
