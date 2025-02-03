251a86b0710470abf86da6d2d26a19ff41228828
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-02-03 18:32:47 +0100

    feat: integrate Setup_Hugo_App for Hugo project setup in static application template

--- src/templates/static.template.ts
+++ src/templates/static.template.ts
@@ -1,5 +1,6 @@
 import { error } from "console";
 import { setup_Gatsby_App } from "./static/gatsby.template.js";
+import { Setup_Hugo_App } from "./static/hugo.template.js";
 
 
 
@@ -7,7 +8,6 @@ export const Setup_Static_App = (flag:string, projectName:string) => {
         // call a switch case on the flag :
         switch(flag){
             case "Gatsby":
-                console.log("Setting up Gatsby");
                 const isGatsbySetup:boolean = setup_Gatsby_App(projectName);
                 if(!isGatsbySetup){
                     console.error("Failed to setup a Gatsby static project :( ", error);
@@ -15,7 +15,11 @@ export const Setup_Static_App = (flag:string, projectName:string) => {
                 }
             break;
             case "Hugo":
-                console.log("Setting up Hugo");
+                const isHugoSetup:boolean = Setup_Hugo_App(projectName);
+                if(!isHugoSetup){
+                    console.error("Failed to setup a Hugo static project :( ", error);
+                    return;
+                }
             break;
             case "Astro":
                 console.log("Setting up Astro");
@@ -25,3 +29,4 @@ export const Setup_Static_App = (flag:string, projectName:string) => {
                 return;
         };
 };
+
